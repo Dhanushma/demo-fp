ae21585618bd35c5daee4f5b4a7b7e71467fb99b
dhanukd <dhnaushmadinesan@gmail.com>
2025-02-17 19:30:26 +0400

    Initial commit

---
+++ src/main/java/com/example/demo_fp/DemoFpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_fp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_fp/DemoFpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_fp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
